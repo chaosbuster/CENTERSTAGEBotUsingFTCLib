e8dc393697cad9337ab4aa175b490bad3edecd40
ChaosBuster <jeanette@techno-chaos.com>
2024-10-08 13:41:46 -0600

    Problem with using "addSequential" method in the CommandOpMode, probably depreciated.  Commented out for now and will need to research how to run commands sequentially for autonomous modes.

--- TeamCode/src/main/java/teamcode/AutoOpMode.java
+++ TeamCode/src/main/java/teamcode/AutoOpMode.java
@@ -1,7 +1,7 @@
 package teamcode;
 
-import com.arcrobotics.ftclib.command.old.CommandOpMode;
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import ftclib.command.CommandOpMode;
+import ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import teamcode.Subsystems.DriveSubsystem;
@@ -18,7 +18,6 @@ public class AutoOpMode extends CommandOpMode {
 
     @Override
     public void initialize() {
-        driverGamepad = new GamepadEx(gamepad1);
         driveSubsystem = new DriveSubsystem(hardwareMap);
 
         driveSubsystem.initialize();
@@ -27,18 +26,18 @@ public class AutoOpMode extends CommandOpMode {
     @Override
     public void run() {
         // Drive Forward for 10 inches with a timeout of 4 seconds.
-        addSequential(new DriveDistance(10, 0.5, driveSubsystem), 2);
+        //addSequential(new DriveDistance(10, 0.5, driveSubsystem), 2);
         // Turn 90 degrees with a timeout of 2 seconds
-        addSequential(new TurnAngleCommand(90, driveSubsystem), 2);
+        //addSequential(new TurnAngleCommand(90, driveSubsystem), 2);
 
         // DO SOME ACTION
 
         sleep(500);
 
         // Drive Forward for -10 inches with a timeout of 4 seconds.
-        addSequential(new DriveDistance(-10, 0.5, driveSubsystem), 4);
+        //addSequential(new DriveDistance(-10, 0.5, driveSubsystem), 4);
 
-        addSequential(new DriveDistance(10, 0.75, driveSubsystem), 3);
+        //addSequential(new DriveDistance(10, 0.75, driveSubsystem), 3);
 
     }
 }
